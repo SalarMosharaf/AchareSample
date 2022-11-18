44c2d63564abc45854319a9302b8142cd8e8a9d8
SalaR Mosharaf <esm@SalaR-MacBook.local>
2022-11-19 01:51:49 +0330

    Navbar & Header complete

--- achare_sample/src/components/navbar/Navbar.jsx
+++ achare_sample/src/components/navbar/Navbar.jsx
@@ -1,17 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import logo from '../../assets/logo.png';
+import signin from '../../assets/signup.png';
 import './navbar.css';
 
 const Navbar = () => {
+
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
     
     <div className="gpt3__navbar">
-    <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links">
+
+          <div className="gpt3__navbar-links_login_logo">
+            <a href="https://www.google.com"> <img src={signin} /> </a>
+          </div>
+
+        </div>
+
       <div className="gpt3__navbar-links_logo">
-        <img src={logo} />
+        <a href="#Home"> <img align="right" src={logo} /> </a>
       </div>
-    </div>
+ 
+      <div className="gpt3__navbar-menu">
+        {toggleMenu
+          ? <RiCloseLine  color="#fff" size={27} onClick={() => setToggleMenu(false)} />
+          : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />}
+        {toggleMenu && (
+        <div className="gpt3__navbar-menu_container scale-up-center">
+          <div className="gpt3__navbar-menu_container-links">
+            <p><a href="#home">Home</a></p>
+            <p><a href="#wgpt3">What is GPT3?</a></p>
+            <p><a href="#possibility">Open AI</a></p>
+            <p><a href="#features">Case Studies</a></p>
+            <p><a href="#blog">Library</a></p>
+          </div>
+          <div className="gpt3__navbar-menu_container-links-login">
+            <a href="https://www.google.com"> <img src={signin} /> </a>
+          </div>
+
+        </div>
+        )}
+      </div>
+
     </div>
   )
 }
--- achare_sample/src/containers/header/Header.jsx
+++ achare_sample/src/containers/header/Header.jsx
@@ -1,11 +1,49 @@
-import React from 'react';
-import './header.css';
+import React, { useState } from "react";
+import "./header.css";
+/*
+import SearchIcon from "@material-ui/icons/Search";
+import CloseIcon from "@material-ui/icons/Close";
+*/
 
+function Header({ placeholder, data }) {
+  const [filteredData, setFilteredData] = useState([]);
+  const [wordEntered, setWordEntered] = useState("");
 
-const Header = () => {
+  const handleFilter = (event) => {
+    const searchWord = event.target.value;
+    setWordEntered(searchWord);
+    /*
+    const newFilter = data.filter((value) => {
+      return value.title.toLowerCase().includes(searchWord.toLowerCase());
+    });
+
+    if (searchWord === "") {
+      setFilteredData([]);
+    } else {
+      setFilteredData(newFilter);
+    }
+    */
+  };
+
+  const clearInput = () => {
+    /*
+    setFilteredData([]);
+    setWordEntered("");
+    */
+  };
+
   return (
-    <div>Header</div>
-  )
-}
+    <div className="search">
+      <div className="searchInputs">
+        <input
+          type="text"
+          placeholder={placeholder}
+          value={wordEntered}
+          onChange={handleFilter}
+        />
 
+      </div>
+    </div>
+  );
+}
 export default Header
